1cae3c4ec08681ce1c4e28b05e56ddae8cab6443
Flavian Alexander <alexandruflavian23@gmail.com>
2013-07-12 17:38:55 +0100

    Euler-11#Problem solved.

--- src/main/scala/org/projecteuler/exercises/LargetProductInAGrid.scala
+++ src/main/scala/org/projecteuler/exercises/LargetProductInAGrid.scala
@@ -15,10 +15,15 @@ object LargetProductInAGrid {
       })
       data += numbers
     })
-
-    data.foreach(line => {
-      println(line(0))
-    })
+    var max = 0;
+    for {
+      x <- List.range(0, data.length);
+      y <- List.range(0, data.length)
+    } {
+      val list = up(x, y) :: down(x, y) :: left(x, y) :: right(x, y) :: upRight(x, y) :: upLeft(x, y) :: downLeft(x, y) :: downRight(x, y) :: Nil
+      if (list.max > max) max = list.max
+    }
+    println(max)
   }
 
   def max: Int = {
@@ -28,7 +33,7 @@ object LargetProductInAGrid {
 
   def up(x: Int, y: Int): Int = {
     if (x + 4 < data.length) {
-      data(x)(y) + data(x + 1)(y) + data(x + 2)(y) + data(x + 3)(y)
+      data(x)(y) * data(x + 1)(y) * data(x + 2)(y) * data(x + 3)(y)
     } else {
       0
     }
@@ -36,7 +41,7 @@ object LargetProductInAGrid {
 
   def down(x: Int, y: Int): Int = {
     if (x - 4 >= 0) {
-      data(x)(y) + data(x - 1)(y) + data(x - 2)(y) + data(x - 3)(y)
+      data(x)(y) * data(x - 1)(y) * data(x - 2)(y) * data(x - 3)(y)
     } else {
       0
     }
@@ -44,7 +49,7 @@ object LargetProductInAGrid {
 
   def left(x: Int, y: Int): Int = {
     if (y - 4 >= 0) {
-      data(x)(y) + data(x)(y - 1) + data(x)(y - 3) + data(x)(y - 4)
+      data(x)(y) * data(x)(y - 1) * data(x)(y - 3) * data(x)(y - 4)
     } else {
       0
     }
@@ -52,7 +57,39 @@ object LargetProductInAGrid {
 
   def right(x: Int, y: Int): Int = {
     if (y + 4 < data(0).length) {
-      data(x)(y) + data(x)(y + 1) + data(x)(y + 3) + data(x)(y + 4)
+      data(x)(y) * data(x)(y + 1) * data(x)(y + 3) * data(x)(y + 4)
+    } else {
+      0
+    }
+  }
+
+  def upRight(x: Int, y: Int): Int = {
+    if (y + 4 < data(0).length && x + 4 < data(0).length) {
+      data(x)(y) * data(x + 1)(y + 1) * data(x + 2)(y + 2) * data(x + 3)(y + 3)
+    } else {
+      0
+    }
+  }
+
+  def upLeft(x: Int, y: Int): Int = {
+    if (y - 4 >= 0 && x + 4 < data(0).length) {
+      data(x)(y) * data(x + 1)(y - 1) * data(x + 2)(y - 2) * data(x + 3)(y - 3)
+    } else {
+      0
+    }
+  }
+
+  def downLeft(x: Int, y: Int): Int = {
+    if (y - 4 > 0 && x - 4 > 0) {
+      data(x)(y) * data(x - 1)(y - 1) * data(x - 2)(y - 2) * data(x - 3)(y - 3)
+    } else {
+      0
+    }
+  }
+
+  def downRight(x: Int, y: Int): Int = {
+    if (y + 4 < data(0).length && x - 4 > 0) {
+      data(x)(y) * data(x - 1)(y + 1) * data(x - 2)(y + 2) * data(x - 3)(y + 3)
     } else {
       0
     }
