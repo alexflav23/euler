735c9ffeb3f183664b4322b0aa492e28b1b018d4
Flavian Alexander <alexandruflavian23@gmail.com>
2013-07-15 14:23:40 +0100

    euler-14#Problem solved.

---
+++ src/main/scala/org/projecteuler/exercises/LargestCollatzSeq.scala
@@ -0,0 +1,41 @@
+package org.projecteuler.exercises
+
+import scala.annotation.tailrec;
+
+import scala.collection.mutable.HashMap;
+object LargestCollatzSeq {
+
+  val map: HashMap[BigInt, BigInt] = new HashMap
+  val limit = 1000000
+
+  @tailrec
+  def rec(x: BigInt, count: BigInt = 1, original: BigInt): BigInt = {
+    if (x == 1) {
+      map.put(original, count)
+      count
+    } else {
+      map.get(x) match {
+        case Some(data) =>
+          map.put(original, count + data - 1)
+          count + data - 1
+        case None => (x % 2).toInt match {
+          case 0 => rec(x / 2, count + 1, original)
+          case 1 => rec(3 * x + 1, count + 1, original)
+        }
+      }
+    }
+  }
+
+  def largestSeq = {
+    var max: BigInt = 0;
+    var number: BigInt = 0;
+    for (i <- 2 to limit) {
+      val c = rec(i, 1, i);
+      if (c > max) {
+        number = i;
+        max = c
+      }
+    }
+    println(s"$number -> $max")
+  }
+}
--- src/main/scala/org/projecteuler/run/Main.scala
+++ src/main/scala/org/projecteuler/run/Main.scala
@@ -1,12 +1,16 @@
 package org.projecteuler.run
 
+import _root_.java.util.concurrent.TimeUnit;
 import org.projecteuler.exercises._;
 
 object Main extends App {
 
   override def main(args: Array[String]) {
     //LargetProductInAGrid.max
-    LargeNumberSum.test
+    val s = System.nanoTime
+    LargestCollatzSeq.largestSeq
+    val f = System.nanoTime
+    println((f - s) / 1000000000.0)
   }
 
 }
