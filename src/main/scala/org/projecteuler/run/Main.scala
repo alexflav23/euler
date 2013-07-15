402e5568650bb9220be13a5329250d871e5f268f
Flavian Alexander <alexandruflavian23@gmail.com>
2013-07-15 11:08:22 +0100

    Added ignore file.

--- src/main/scala/org/projecteuler/run/Main.scala
+++ src/main/scala/org/projecteuler/run/Main.scala
@@ -4,6 +4,7 @@ import org.projecteuler.exercises._;
 
 object Main extends App {
   override def main(args: Array[String]) {
-    LargetProductInAGrid.max
+    //LargetProductInAGrid.max
+    HighlyDivisibleTriangularNumber.smallestNumber
   }
 }
