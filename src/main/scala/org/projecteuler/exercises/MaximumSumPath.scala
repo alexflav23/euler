a8e440c8c0791f4114a3d45b614a1b0c7817b5c6
Flavian Alexander <alexandruflavian23@gmail.com>
2013-07-16 13:58:50 +0100

    Updating solution

--- src/main/scala/org/projecteuler/exercises/MaximumSumPath.scala
+++ src/main/scala/org/projecteuler/exercises/MaximumSumPath.scala
@@ -7,16 +7,9 @@ object MaximumSumPath {
 
   val triangle: ListBuffer[ListBuffer[Int]] = new ListBuffer
 
-  def read = {
-    Source.fromFile("triangle.in").getLines.foreach(triangle += _.split("""\s+""").map(_.toInt).toList.to[ListBuffer])
-  }
-
   def dynamic: Int = {
-    read
-    for (
-      i <- triangle.length - 2 to 0 by -1;
-      j <- 0 to i
-    ) {
+    Source.fromFile("triangle.in").getLines.foreach(triangle += _.split("""\s+""").map(_.toInt).toList.to[ListBuffer])
+    for (i <- triangle.length - 2 to 0 by -1; j <- 0 to i) {
       triangle(i)(j) += math.max(triangle(i + 1)(j), triangle(i + 1)(j + 1))
     }
     triangle(0)(0)
