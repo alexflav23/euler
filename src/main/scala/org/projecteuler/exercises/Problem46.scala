60f7a1c0c26b384981a22c9be0f46626dfc853d2
Flavian Alexander <alexandruflavian23@gmail.com>
2013-07-17 17:56:41 +0100

    Euler-46#Problem solved.

--- src/main/scala/org/projecteuler/exercises/Problem46.scala
+++ src/main/scala/org/projecteuler/exercises/Problem46.scala
@@ -2,47 +2,47 @@ package org.projecteuler.exercises
 
 object Problem46 {
   def isComposite(n: Int): Boolean = {
+    var test = false;
     for (i <- 2 to math.sqrt(n).toInt) {
       if (n % i == 0)
-        return true
+        test = true
     }
-    false
+    test
   }
 
-  def hasSquare(x: Int): Boolean = {
-    for (i <- 1 to x; j <- 1 to x) {
-      if (isSquare(j / 2))
-        return true;
+  def isPrime(n: Int): Boolean = {
+    var test = true;
+    for (i <- 2 to math.sqrt(n).toInt) {
+      if (n % i == 0)
+        test = false
     }
-    false
-  }
-
-  def isSquare(x: Int) = {
-    (x & 0xF).toInt match {
-      case 0 => math.pow(math.sqrt(x).toInt, 2) == x
-      case 1 => math.pow(math.sqrt(x).toInt, 2) == x
-      case 4 => math.pow(math.sqrt(x).toInt, 2) == x
-      case 9 => math.pow(math.sqrt(x).toInt, 2) == x
-      case _ => false
-    }
+    if (n == 1) test = false
+    test
   }
 
   def isConjecture(x: Int): Boolean = {
-    hasSquare(x)
+    for (i <- 1 to x) {
+      val j = x - i
+      if (isSquare(j / 2) && isPrime(i)) {
+        val z = math.sqrt(j / 2).toInt
+        //println(s"$x = 2 * $z\u00B2  + $i")
+        return true;
+      }
+    }
+    false
+  }
+
+  def isSquare(x: Int): Boolean = {
+    val z = math.sqrt(x).toInt
+    z * z == x
   }
 
   def compute: Int = {
-    //for (i <- 1 to 27) { if (i % 2 == 1) println(i + ": " + isComposite(i)) }
-    var i = 27;
-    var test = true;
-    while (test == true) {
-      if (isComposite(i)) {
-        test = hasSquare(i)
-        println(i)
-        println(test)
+    for (i <- 1 to 10000 by 2) {
+      if (isComposite(i) && !isConjecture(i)) {
+        return i
       }
-      i += 2
     }
-    i
+    0
   }
 }
