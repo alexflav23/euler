c0ba14321052cd5ae4968f088c46040db674c516
Flavian Alexander <alexandruflavian23@gmail.com>
2013-07-18 19:02:50 +0100

    Euler-35#Setup

---
+++ src/main/scala/org/projecteuler/exercises/Problem35.scala
@@ -0,0 +1,31 @@
+package org.projecteuler.exercises
+
+object Problem35 {
+
+  def primesUnder(n: Int): List[Int] = {
+    require(n >= 2)
+
+    def rec(i: Int, primes: List[Int]): List[Int] = {
+      if (i >= n) primes
+      else if (prime(i, primes)) rec(i + 1, i :: primes)
+      else rec(i + 1, primes)
+    }
+
+    rec(2, List()).reverse
+  }
+
+  def prime(num: Int, factors: List[Int]): Boolean = factors.forall(num % _ != 0)
+
+  def compute: Int = {
+    var count = 0;
+
+    val sieve = primesUnder(1000000);
+    val perms = Iterator
+    sieve foreach (prime => {
+
+    })
+
+    count
+  }
+
+}
