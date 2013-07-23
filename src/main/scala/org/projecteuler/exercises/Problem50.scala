7c0f57107dd9c7192a228774d6cc9c65ef6295c7
Flavian Alexander <alexandruflavian23@gmail.com>
2013-07-23 11:38:52 +0100

    Euler-50#Solvedf.

--- src/main/scala/org/projecteuler/exercises/Problem50.scala
+++ src/main/scala/org/projecteuler/exercises/Problem50.scala
@@ -2,4 +2,21 @@ package org.projecteuler.exercises
 
 object Problem50 {
 
+  lazy val ps: Stream[Int] = 2 #:: Stream.from(3).filter(i =>
+    ps.takeWhile { j => j * j <= i }.forall { k => i % k > 0 });
+
+  def compute(sum: Int = 0, max: Int = 0, primes: Stream[Int] = ps, terms: List[Int] = Nil): Int = {
+    if (sum < 1000000) {
+      val test = Problem46.isPrime(sum)
+      println(sum + " " + terms.size)
+      if (test == true) {
+        compute(sum + primes.head, sum, primes.tail, primes.head :: terms)
+      } else {
+        compute(sum + primes.head, max, primes.tail, primes.head :: terms)
+      }
+    } else {
+      max
+    }
+  }
+
 }
