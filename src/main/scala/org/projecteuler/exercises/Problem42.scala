90ea83903b1db8f3446bdf0c8766bc569dce6f1a
Flavian Alexander <alexandruflavian23@gmail.com>
2013-07-29 14:26:01 +0100

    Euler-42#Solved.

--- src/main/scala/org/projecteuler/exercises/Problem42.scala
+++ src/main/scala/org/projecteuler/exercises/Problem42.scala
@@ -1,5 +1,24 @@
 package org.projecteuler.exercises
 
-object Problem42 {
+import scala.io.Source;
+import scala.collection.mutable.ListBuffer;
 
+object Problem42 {
+  val words = Source.fromFile("words.txt").mkString.split("""[\",\"]""") dropWhile (_.matches("""\s"""))
+  val wordList: ListBuffer[String] = new ListBuffer();
+
+  def getWordValue(word: String): Int = {
+    var sum = 0;
+    word foreach (char => { sum += char.toLower - 96 })
+    sum;
+  }
+
+  def isTriangular(x: Int): Boolean = {
+    val sq = math.sqrt(8 * x + 1)
+    sq == sq.toInt && x != 0
+  }
+
+  def compute: Int = {
+    words.filter(word => { isTriangular(getWordValue(word)) }).size
+  }
 }
