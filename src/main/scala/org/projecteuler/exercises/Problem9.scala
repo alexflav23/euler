0fc955dbb4bb18b19314618a37a6647b08d15b06
Flavian Alexander <alexandruflavian23@gmail.com>
2013-07-29 18:19:31 +0100

    Euler-9#Solved.

--- src/main/scala/org/projecteuler/exercises/Problem9.scala
+++ src/main/scala/org/projecteuler/exercises/Problem9.scala
@@ -2,4 +2,13 @@ package org.projecteuler.exercises
 
 object Problem9 {
 
+  def compute: Int = {
+    for (a <- 1 to 1000; b <- 1 until a) {
+      val c = 1000 - a - b;
+      if (math.pow(c, 2) == math.pow(a, 2) + math.pow(b, 2) && (a + b + c == 1000)) {
+        return a * b * c;
+      }
+    }
+    1
+  }
 }
--- src/main/scala/org/projecteuler/run/Main.scala
+++ src/main/scala/org/projecteuler/run/Main.scala
@@ -8,7 +8,7 @@ object Main extends App {
   override def main(args: Array[String]) {
     //LargetProductInAGrid.max
     val s = System.nanoTime
-    println("Result: " + Problem8.compute)
+    println("Result: " + Problem9.compute)
 
     val f = System.nanoTime
     println("Solution took " + (f - s) / 1000000 + " miliseconds")
