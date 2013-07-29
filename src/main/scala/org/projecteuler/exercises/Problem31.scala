e3f07a503aa75aebd8b81dfde03604a1d5c6275d
Flavian Alexander <alexandruflavian23@gmail.com>
2013-07-29 15:09:30 +0100

    Euler-31#Solved, Euler-32#Solved

--- src/main/scala/org/projecteuler/exercises/Problem31.scala
+++ src/main/scala/org/projecteuler/exercises/Problem31.scala
@@ -2,4 +2,13 @@ package org.projecteuler.exercises
 
 object Problem31 {
 
+  val target = 200;
+  def compute: Int = {
+    var ways = 0;
+    for (a <- target to 0 by -200; b <- a to 0 by -100; c <- b to 0 by -50; d <- c to 0 by -20; e <- d to 0 by -10; f <- e to 0 by -5; g <- f to 0 by -2) {
+      ways += 1;
+    }
+
+    ways
+  }
 }
--- src/main/scala/org/projecteuler/exercises/Problem45.scala
+++ src/main/scala/org/projecteuler/exercises/Problem45.scala
@@ -2,4 +2,18 @@ package org.projecteuler.exercises
 
 object Problem45 {
 
+  def isPentagonal(x: Long): Boolean = {
+    ((math.sqrt(24 * x + 1) + 1) / 6).isWhole
+  }
+
+  def compute: Long = {
+    var i: Long = 143;
+    while (1 != 2) {
+      i += 1;
+      if (isPentagonal(i * (2 * i - 1))) {
+        return i * (2 * i - 1)
+      }
+    }
+    0
+  }
 }
