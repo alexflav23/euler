16a521be054b6e6f18cd3da8ce27e80ddf44e65b
Flavian Alexander <alexandruflavian23@gmail.com>
2013-07-29 18:07:09 +0100

    Euler-7#Solved

--- src/main/scala/org/projecteuler/exercises/Problem7.scala
+++ src/main/scala/org/projecteuler/exercises/Problem7.scala
@@ -2,4 +2,10 @@ package org.projecteuler.exercises
 
 object Problem7 {
 
+  lazy val ps: Stream[Int] = 2 #:: Stream.from(3).filter(i =>
+    ps.takeWhile { j => j * j <= i }.forall { k => i % k > 0 });
+
+  def compute: Int = {
+    ps drop 10000 head
+  }
 }
