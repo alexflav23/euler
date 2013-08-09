5a7e61e88f5149d3f8dd3a678dfacb784ef8b6bb
Flavian Alexander <alexandruflavian23@gmail.com>
2013-08-09 13:18:41 +0100

    Adding sun java rpm.

--- src/main/scala/org/projecteuler/run/Main.scala
+++ src/main/scala/org/projecteuler/run/Main.scala
@@ -9,28 +9,9 @@ object Main extends App {
   override def main(args: Array[String]) {
 
     val s = System.nanoTime
-    println("Result: " + Problem24.compute)
+    println("Result: " + Problem34.compute)
     val f = System.nanoTime
 
     println("Solution took " + (f - s) / 1000000 + " miliseconds")
-
-    val str = ",,,,hello,,,,"
-    val str2 = ",,,,hello,,,,  "
-
-    def trim(str: String): String = {
-      str.replaceAll("""[\s,]""", "");
-    }
-
-    def trimLeft(str: String): String = {
-      str.replaceAll("""^[\s,]+""", "")
-    }
-
-    def trimRight(str: String): String = {
-      str.replaceAll("""[\s,]+$""", "")
-    }
-
-    println("Left: " + trimLeft(str2));
-    println("Right: " + trimRight(str));
-
   }
 }
